cd5452d9e8de4b0e2c5f688498dac8ae007b537c
gabrielgits <gvgabrielvieiragabrielvieira@gmail.com>
2025-01-01 09:59:10 +0100

    fixing error on update app

--- lib/src/shared/screens/show_error_view.dart
+++ lib/src/shared/screens/show_error_view.dart
@@ -1,8 +1,13 @@
 import 'package:bform/bform.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ftcontrol/ftcontrol.dart';
+import 'package:go_router/go_router.dart';
 
-class ShowErrorView extends StatelessWidget {
+import '../../features/auth/ui/view_models/auth_view_model.dart';
+
+class ShowErrorView extends ConsumerWidget {
   const ShowErrorView({
     super.key,
     required this.detail,
@@ -16,21 +21,24 @@ class ShowErrorView extends StatelessWidget {
   final VoidCallback onPressed;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Column(
       children: [
         Text(title, style: Theme.of(context).textTheme.titleLarge),
         const SizedBox(height: 16),
         FtcontrolView(
-          text: detail,
-          type: FtcontrolType.danger,
+          text: tr('messages.logoutMsn'),
+          type: FtcontrolType.warning,
         ),
         const SizedBox(height: 16),
         BformButton(
-          label: buttonText,
+          label: tr('messages.logoutBtn'),
           fontSize: 16,
           colors: [Theme.of(context).primaryColor],
-          onPressed: onPressed,
+          onPressed: () {
+            context.replaceNamed('login');
+            ref.read(authViewModelProvider.notifier).logout();
+          },
         ),
       ],
     );
